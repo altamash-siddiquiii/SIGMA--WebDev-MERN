a4d9996fd2a0cf3cb94c126aa64b7e996b4171bf
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2024-08-15 00:54:37 +0530

    Add a new code for handling deletion

--- 3 Database/3 Database Relationships/Models/customer.js
+++ 3 Database/3 Database Relationships/Models/customer.js
@@ -32,6 +32,22 @@ const customerSchema = new Schema({
     ]
 });
 
+// Handling Deletion - using Mongoose Middlewares
+
+// Pre middleware
+// customerSchema.pre("findOneAndDelete", async () => {
+//     console.log("Pre Middleware");
+// });
+
+// Post middleware - Runs when a customer is deleted, all associated orders are also deleted
+customerSchema.post("findOneAndDelete", async (cust) => {
+    if (cust.orders.length) {
+        // Delete all orders whose _id is present in the cust.orders array
+        let res = await Order.deleteMany({ _id: { $in: cust.orders } });
+        console.log("Deleted associated orders:", res);
+    }
+});
+
 // Creating models for Order and Customer collections
 const Order = mongoose.model("Order", orderSchema);
 const Customer = mongoose.model("Customer", customerSchema);
@@ -72,4 +88,29 @@ const findCustomers = async () => {
     let customers = await Customer.find().populate("orders");
     console.log(customers[0]); // Logging the first customer with populated orders
 }
-findCustomers(); // Calling the function to find and display customers with their orders
+// findCustomers(); // Calling the function to find and display customers with their orders
+
+// New async function to add a new customer and order for handling deletion using middlewares
+const addNewCust = async () => {
+    let newCust = new Customer({
+        custName: "Karan"
+    });
+    let newOrder = new Order({
+        itemName: "Pizza",
+        price: 250
+    });
+    newCust.orders.push(newOrder);
+
+    await newOrder.save();
+    await newCust.save();
+
+    console.log("Added successfully.");
+}
+// addNewCust();
+
+// Async function to delete a customer and trigger the post middleware
+const delCust = async () => {
+    let del = await Customer.findByIdAndDelete("66bca72f5dcb511857bf0ed6");
+    console.log("Deleted Customer =", del);
+}
+delCust(); // Call the function to delete the customer and their associated orders
